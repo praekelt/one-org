1083b3e4e5e24f8ce87569ac560b5c8f89afb53a
Darryn <pptdar001@myuct.ac.za>
2014-12-15 13:03:50 +0200

    Added email field to SignPetitionForm

--- org/models.py
+++ org/models.py
@@ -21,7 +21,17 @@ class Signup(models.Model):
 
 class PetitionEntry(models.Model):
     name = models.CharField(max_length=255)
-    mobile_number = models.CharField(max_length=16, unique=True)
+    mobile_number = models.CharField(
+        max_length=16,
+        unique=True,
+        blank=True
+    )
+    email = models.CharField(
+        max_length=64,
+        unique=True,
+        validators=[validate_email],
+        blank=True
+    )
     created = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
