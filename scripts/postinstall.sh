11593244eb26333e01f111ea6c090183e573727c
Colin Alston <colin.alston@gmail.com>
2014-12-22 12:51:35 +0200

    Fix up deploy settings

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,10 +1,15 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
-$manage syncdb --noinput --no-initial-data
-$manage migrate --noinput
-$manage collectstatic --noinput
-
 if [ ! -f ${INSTALLDIR}/one-org-installed ]; then
-    $manage loaddata ${INSTALLDIR}/${REPO}/one/fixtures/sites.json
+    su - postgres -c "createdb org_live"
+    su - postgres -c "psql org_live -c 'CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;'"
+
+    $manage syncdb --noinput
+    $manage migrate --noinput
+    $manage collectstatic --noinput
+    $manage loaddata ${INSTALLDIR}/${REPO}/org/fixtures/sites.json
     touch ${INSTALLDIR}/one-org-installed
+else
+    $manage migrate --noinput
+    $manage collectstatic --noinput
 fi
